e70d48f0f98ea2fac853f8786d292dba40c5ba6a
Rui Peixoto <1191587@isep.ipp.pt>
2021-10-03 19:07:31 +0100

    configures stockAndPrice(producer) to send messages as JSON

---
+++ StockAndPrice/src/main/java/com/ruipeixoto/stockandprice/configs/ConfigurationToSendJSON.java
@@ -0,0 +1,24 @@
+package com.ruipeixoto.stockandprice.configs;
+
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ConfigurationToSendJSON {
+
+    //makes rabbitTemplate convert messages to JSON by default
+    @Bean
+    public RabbitTemplate rabbitTemplate(final ConnectionFactory connectionFactory) {
+        final RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(producerJackson2MessageConverter());
+        return rabbitTemplate;
+    }
+
+    @Bean
+    public Jackson2JsonMessageConverter producerJackson2MessageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+}
